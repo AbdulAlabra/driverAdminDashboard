de7352486ed2405810ac94c41c8da516413cf870
Abdulmohsen Alabra <abdulmohsenalabra@Abdulmohsens-MacBook-Pro.local>
2020-06-18 12:42:23 -0700

    profile change

--- src/layouts/Main/components/Sidebar/components/Profile/Profile.js
+++ src/layouts/Main/components/Sidebar/components/Profile/Profile.js
@@ -27,9 +27,9 @@ const Profile = props => {
   const classes = useStyles();
 
   const user = {
-    name: 'Shen Zhi',
+    name: 'Abdulmohsen Alabra',
     avatar: '/images/avatars/avatar_11.png',
-    bio: 'Brain Director'
+    bio: 'Founder'
   };
 
   return (
